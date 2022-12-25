d96c35369b0e3b778897c5d07014cd403f33a3f0
Wamiq <wamiqsiddiqui30@gmail.com>
2022-12-25 19:23:35 +0500

    call model created

---
+++ lib/Model/call.dart
@@ -0,0 +1,41 @@
+class Call {
+  final String callerId;
+  final String callerName;
+  final String callerPic;
+  final String receiverId;
+  final String receiverName;
+  final String receiverPic;
+  final String channelId;
+  final bool hasDialed;
+
+  Call(
+      {required this.callerId,
+      required this.callerName,
+      required this.callerPic,
+      required this.receiverId,
+      required this.receiverName,
+      required this.receiverPic,
+      required this.channelId,
+      required this.hasDialed});
+
+  factory Call.fromMap(Map<String, dynamic> map) => Call(
+      callerId: map['callerId'],
+      callerName: map['callerName'],
+      callerPic: map['callerPic'],
+      receiverId: map['receiverId'],
+      receiverName: map['receiverName'],
+      receiverPic: map['receiverPic'],
+      channelId: map['channelId'],
+      hasDialed: map['hasDialed']);
+
+  Map<String, dynamic> toMap() => {
+        'callerId': callerId,
+        'callerName': callerName,
+        'callerPic': callerPic,
+        'receiverId': receiverId,
+        'receiverName': receiverName,
+        'receiverPic': receiverPic,
+        'channelId': channelId,
+        'hasDialed': hasDialed
+      };
+}
